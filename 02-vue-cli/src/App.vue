0d89b0819a9924161e359c14fe266bfd1777d4a8
summer <summery.dev@gmail.com>
2022-10-24 09:33:35 +0900

    create vue project

---
+++ 02-vue-cli/src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <AppHeader v-bind:propsdata="headerDes" v-on:renew="renewStr"></AppHeader>
+  </div>
+</template>
+
+<script>
+import AppHeader from "./components/AppHeader.vue";
+export default {
+  data: function () {
+    return {
+      headerDes: "hi",
+    };
+  },
+  components: { AppHeader },
+  methods: {
+    renewStr: function () {
+      this.headerDes = "headerDes changed";
+    },
+  },
+};
+</script>
+
+<style>
+</style>
---
+++ 02-vue-cli/src/components/AppHeader.vue
@@ -0,0 +1,24 @@
+<template>
+  <header>
+    <h1>Header</h1>
+    <div>
+      {{ propsdata }}
+
+      <button v-on:click="clickEvent">click</button>
+    </div>
+  </header>
+</template>
+
+<script>
+export default {
+  props: ["propsdata"],
+  methods: {
+    clickEvent: function () {
+      this.$emit("renew");
+    },
+  },
+};
+</script>
+
+<style>
+</style>
